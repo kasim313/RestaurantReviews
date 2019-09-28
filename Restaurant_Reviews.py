9ed5e97ec499cff5ae6bedf0051e50a0809268ce
MohdKasim <mohdkasim063@gmail.com>
2019-09-28 13:53:42 +0530

    two fiels are being added

--- Restaurant_Reviews.py
+++ Restaurant_Reviews.py
@@ -3,6 +3,10 @@
 # Source Geeks for geeks
   
 # Importing packages 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer 
 from nltk.tokenize import word_tokenize
@@ -12,9 +16,9 @@ import sys
 import os 
 import re
 
-
 dataset = pd.read_csv("Restaurant_Reviews.tsv", delimiter = "\t")   #Importing data sets 
 def textCleansing(data):
+    courpus = []
     for i in range(0,len(data)):
         reviews = data['Review'][i].lower() #Converting it into lower case   
         reviews = re.sub(r"\W+"," ",reviews) #Removeing special charector
@@ -23,7 +27,15 @@ def textCleansing(data):
         ps = PorterStemmer()#Creating instance of port stemming class
         reviews = [ps.stem(w) for w in reviews if w not in set(stopwords.words('english'))] #Stemming or removeing stor words 
         reviews = " ".join(reviews)# Rejoin words into string  
-            
-              
+        courpus.append(reviews)#Appending into list    
+    return courpus
 
-textCleansing(dataset)
+cv = CountVectorizer()#Creating instance of countvectorizer
+X = cv.fit_transform(textCleansing(dataset)).toarray() #Input varaible
+Y = dataset.iloc[:,1].values #Out put varaible
+X_train , X_test ,Y_train, Y_test = train_test_split(X,Y, test_size = 0.25) #Spliting data sets into trains
+model = RandomForestClassifier(n_estimators = 501 , criterion= 'entropy')
+model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
+cm = confusion_matrix(Y_test,y_pred)
+print(cm)
